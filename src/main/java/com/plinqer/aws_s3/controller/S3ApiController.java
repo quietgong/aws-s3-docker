4d296e48b774dece376a2c4e4189f86796d78888
quietgong <quietgong@naver.com>
2025-01-11 23:29:04 +0900

    edit

--- src/main/java/com/plinqer/aws_s3/controller/S3ApiController.java
+++ src/main/java/com/plinqer/aws_s3/controller/S3ApiController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", methods = RequestMethod.POST)
-@RequestMapping("/file")
+//@RequestMapping("/file")
 @RequiredArgsConstructor
 public class S3ApiController {
 
